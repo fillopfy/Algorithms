e13f3103224243257f42b7c8c72b6f287815f21c
Abhishek Tiwari <abhi456tm@gmail.com>
2019-12-19 19:25:53 +0530

    Updating Bubble sort for input from user

--- BubbleSort.java
+++ BubbleSort.java
@@ -10,6 +10,9 @@ public class BubbleSort {
 		int n=sc.nextInt();
 		
 		int[] arr = new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
 		
 		//flag to break from loop to remove replication of passes if everything gets sorted before n-1 passes
 		boolean flag;
